5de3b488a18eb1c478b24324614d77ea5833a77b
Roman Govorov <roman@govorov.top>
2022-02-01 09:31:16 +0300

    Lern_2

--- 1. Основы программирования/10. Оператор break/scripts.js
+++ 1. Основы программирования/10. Оператор break/scripts.js
@@ -1,1 +1,11 @@
 // Оператор break
+
+while (true){
+   const age = +prompt('How old are you?');
+   if (age){
+       alert(`You are ${age} years old!`);
+       break;
+   }else {
+       alert('Enter a number!');
+   }
+}
