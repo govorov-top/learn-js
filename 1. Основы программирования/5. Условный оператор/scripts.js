00708ca2b958614269c3bbda2f4aa762a01a31a7
Roman Govorov <roman@govorov.top>
2022-02-01 08:51:24 +0300

    Lern_2

--- 1. Основы программирования/5. Условный оператор/scripts.js
+++ 1. Основы программирования/5. Условный оператор/scripts.js
@@ -1,1 +1,19 @@
 // Условные операторы
+let age = 25;
+
+if (age => 18) {
+    console.log('Access granted');
+}else {
+    console.log('Access denied');
+}
+
+let salary = 2000;
+if (salary < 3000) console.log('I need to improve! (Мне нужно совершенствоваться)');
+
+if (salary > 3000) {
+    console.log('It\'s time to relax');
+}else if (salary > 2500){
+    console.log('Well, good enough. But i can be better.');
+}else {
+    console.log('Something else.');
+}
--- 1. Основы программирования/6. Оператор switch/scripts.js
+++ 1. Основы программирования/6. Оператор switch/scripts.js
@@ -1,1 +1,18 @@
-// Логические выражения
+// Оператор switch
+let status = prompt('Enter a status:');
+switch (status) {
+    // Можно так:
+    case '500' : console.log('500'); break;
+    // Можно и так:
+    case '404' : {
+        console.log('404');
+    } break;
+    // Можно и так:
+    case '300' : {
+        console.log('404');
+        break;
+    }
+    case '301' : console.log('301'); break;
+    default : console.log('Welcome!');
+}
+
