7db2a97023d9f05778cc4efc132b5ecb7ac890a8
Roman Govorov <roman@govorov.top>
2022-02-01 09:20:34 +0300

    Lern_2

--- 1. Основы программирования/8. Цикл while/scripts.js
+++ 1. Основы программирования/8. Цикл while/scripts.js
@@ -1,7 +1,6 @@
-// Циклы
+// Цикл while - до тех пора пока
 const arrNum = [1,2,3,4,5,6,7,8,9,10];
 
-// while - до тех пора пока
 let i = 0;
 while (i < arrNum.length){
     console.log(arrNum[i] * 2);
--- 1. Основы программирования/9. Цикл for/scripts.js
+++ 1. Основы программирования/9. Цикл for/scripts.js
@@ -1,1 +1,7 @@
-// Логические выражения
+// Цикл for
+const arrNum = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < arrNum.length; i++){
+    console.log(arrNum[i] * 2);
+}
+console.log('For the end.');
