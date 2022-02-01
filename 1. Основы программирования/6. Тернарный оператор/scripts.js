e42f37a39d280296b44012345f221a9e38269447
Roman Govorov <roman@govorov.top>
2022-02-01 09:01:52 +0300

    Lern_2

--- 1. Основы программирования/6. Тернарный оператор/scripts.js
+++ 1. Основы программирования/6. Тернарный оператор/scripts.js
@@ -1,1 +1,4 @@
-// Логические выражения
+// Тернарный оператор (выражение, а if и else это инструкция)
+const age = 25;
+const access = age >= 18 ? 'Access granted' : 'Access denied';
+console.log('age = 25, проверяем age >= 18: ', access);
