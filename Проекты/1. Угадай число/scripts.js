a301d54d05358d140a2b88f53ceb4a8b2ecf7310
Roman Govorov <roman@govorov.top>
2022-02-02 10:37:49 +0300

    Learn_3

--- Проекты/1. Угадай число/scripts.js
+++ Проекты/1. Угадай число/scripts.js
@@ -1,11 +1,11 @@
 // Проект: Угадай число
 function guessNum(number, tries) {
+    console.log(tries);
     const secretNum = Math.ceil(Math.random() * 10);
-    if (tries >= 5) {
+    if (tries >= 3) {
         alert('Попытки кончились...');
         return true;
     }
-
     if (number === secretNum){
         alert('Угадал(a)!');
         return true;
@@ -17,6 +17,19 @@ function guessNum(number, tries) {
 document.querySelectorAll('button').forEach(el => {
     el.addEventListener('click', function () {
         let number = el.getAttribute('data-number');
-        guessNum(number, 0);
+        if (number){
+            let tries = localStorage.getItem('tries');
+            if (!tries) localStorage.setItem('tries', '3');
+            const result = guessNum(number, tries);
+            if (!result){
+                tries++;
+                localStorage.setItem('tries', tries)
+            }
+        }
     });
+});
+
+document.querySelector('button[data-reset="1"]').addEventListener('click', function () {
+    localStorage.setItem('tries', '0');
+    document.location.reload();
 });
