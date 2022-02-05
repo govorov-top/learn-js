350bcb6bbadd10e77344c2840fff7fc7a089dafc
Roman Govorov <roman@govorov.top>
2022-02-05 21:57:22 +0300

    Learn_5

---
+++ 6. Работа с готовыми библиотеками/1. Библиотека хэлперов lodash/scripts.js
@@ -0,0 +1,15 @@
+// Библиотека хэлперов lodash
+
+// Функция - функция, которая что-то возвращает
+function sum(a,b) {
+    return a + b;
+}
+let res = sum(5,9);
+console.log(res);
+
+// Процедуры - функция, которая ничего не возвращает
+function sumNoReturn(a,b) {
+    console.log(a + b);
+}
+
+res = sumNoReturn(5,9);
---
+++ 7. Асинхронный JavaScript/1. Синхронный и асинхронный код/scripts.js
@@ -0,0 +1,15 @@
+// Синхронный и асинхронный код
+
+// Функция - функция, которая что-то возвращает
+function sum(a,b) {
+    return a + b;
+}
+let res = sum(5,9);
+console.log(res);
+
+// Процедуры - функция, которая ничего не возвращает
+function sumNoReturn(a,b) {
+    console.log(a + b);
+}
+
+res = sumNoReturn(5,9);
