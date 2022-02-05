975f3c1ab50a6a81708d947224413305bd0bffc9
Roman Govorov <roman@govorov.top>
2022-02-05 19:00:56 +0300

    Learn_5

--- 3. Про функции подробно/5. Рекурсия/scripts.js
+++ 3. Про функции подробно/5. Рекурсия/scripts.js
@@ -1,15 +1,30 @@
 // Рекурсия
 
-// Функция - функция, которая что-то возвращает
-function sum(a,b) {
-    return a + b;
-}
-let res = sum(5,9);
-console.log(res);
-
-// Процедуры - функция, которая ничего не возвращает
-function sumNoReturn(a,b) {
-    console.log(a + b);
+// 1. Базовое условия (терминальное) выхода из рекурсии
+// 2. правило движение рекурсии
+function factorial(num) {
+    if (num === 0) return 1;
+    return num * factorial(num - 1);
 }
 
-res = sumNoReturn(5,9);
+console.log(factorial(30)); // 30 * 29 * 28 ...
+
+// function принимает символ и возвращает его в 5 экземплярах
+let counter = 0;
+
+function repeater(symbol, num) {
+    counter++;
+    if (counter === num){
+        counter = 0;
+        return symbol;
+    }
+    return symbol + repeater(symbol, num);
+}
+
+console.log(repeater('x', 10));
+
+function getLength(array) {
+    if (array.pop() === undefined) return 0;
+    return 1 + getLength(array);
+}
+console.log(getLength([1]));
