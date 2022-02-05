abd0f2c0a0b07c0ba4070c13c466433f27bc0cdc
Roman Govorov <roman@govorov.top>
2022-02-05 17:06:06 +0300

    Learn_5

--- 3. Про функции подробно/3. Область видимости/scripts.js
+++ 3. Про функции подробно/3. Область видимости/scripts.js
@@ -1,2 +1,11 @@
-// Область видимости
+// Область видимости - scope
 
+let isValid = false;
+const str = 'Hello';
+
+function isString(str) {
+    const isValid = typeof str === 'string';
+    console.log(isValid);
+    return isValid;
+}
+const result = isString('Привет мир!');
